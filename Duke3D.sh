a2d2da4041a3f15be6d8698efcf216298c81f461
Jordi Sesmero <jordi.sesmeromolina@telefonica.com>
2024-11-13 08:37:10 +0100

    Update Duke3D.sh
    add soundfont

--- Duke3D.sh
+++ Duke3D.sh
@@ -4,6 +4,9 @@ export DUKE_HOME_DIR="/media/fat/duke32"
 
 export DUKE_LIB_PATH="$DUKE_HOME_DIR/arm-linux-gnueabihf:$DUKE_HOME_DIR/arm-linux-gnueabihf/pulseaudio"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$DUKE_LIB_PATH"
+export SDL_SOUNDFONTS=$DUKE_HOME_DIR/SC55.sf2
+export SDL_FORCE_SOUNDFONTS=1
+
 echo $LD_LIBRARY_PATH
 
 export HOME=$DUKE_HOME_DIR
